8f4edb21aaee754fa5c2b3186e108c0881d2207f
berkay.kirecci <irfankirecci@stu.comu.edu.tr>
2025-08-01 02:33:20 +0300

    Initial commit

---
+++ library/build.gradle.kts
@@ -0,0 +1,89 @@
+import com.vanniktech.maven.publish.SonatypeHost
+import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.vanniktech.mavenPublish)
+}
+
+group = "io.github.kotlin"
+version = "1.0.0"
+
+kotlin {
+    jvm()
+    androidTarget {
+        publishLibraryVariants("release")
+        @OptIn(ExperimentalKotlinGradlePluginApi::class)
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_11)
+        }
+    }
+    iosX64()
+    iosArm64()
+    iosSimulatorArm64()
+    linuxX64()
+
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                //put your multiplatform dependencies here
+            }
+        }
+        val commonTest by getting {
+            dependencies {
+                implementation(libs.kotlin.test)
+            }
+        }
+    }
+}
+
+android {
+    namespace = "org.jetbrains.kotlinx.multiplatform.library.template"
+    compileSdk = libs.versions.android.compileSdk.get().toInt()
+    defaultConfig {
+        minSdk = libs.versions.android.minSdk.get().toInt()
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+val userName = extra["GithubPackagesUsername"] as? String
+val name = extra["GithubPackagesName"] as? String
+val email = extra["GithubPackagesEmail"] as? String
+
+mavenPublishing {
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+
+    signAllPublications()
+
+    coordinates(group.toString(), "library", version.toString())
+
+    pom {
+        name = "KMP-Snackbar"
+        description = "Multiplatform Library"
+        inceptionYear = "2025"
+        url.set("https://github.com/$userName/KMP-Snackbar")
+        licenses {
+            license {
+                name.set("MIT")
+                url.set("https://opensource.org/licenses/MIT")
+            }
+        }
+        developers {
+            developer {
+                id = "$userName"
+                name = "$name"
+                url = "$email"
+            }
+        }
+        scm {
+            url = "https://github.com/$userName/arca-framework"
+            connection = "scm:git:git://github.com/$userName.git"
+            developerConnection = "scm:git:ssh://github.com/$userName"
+        }
+    }
+}
